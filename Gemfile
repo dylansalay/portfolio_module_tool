8e3d61beaa218a00298a1c96cf2171fcf6aec681
Dylan Salay <dylansalay@Dylans-MacBook-Pro.local>
2019-11-05 16:47:17 -0800

    portfolio tool initial commit

---
+++ Gemfile
@@ -0,0 +1,4 @@
+source "https://rubygems.org"
+
+# Specify your gem's dependencies in portfolio_module_tool.gemspec
+gemspec
---
+++ lib/portfolio_module_tool.rb
@@ -0,0 +1,7 @@
+require "portfolio_module_tool/version"
+require "portfolio_module_tool/renderer"
+
+module PortfolioModuleTool
+  class Error < StandardError; end
+  # Your code goes here...
+end
